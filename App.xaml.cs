8cea24893ff0ab978571b9815b21c925e83aa79c
MartyIX <203266+MartyIX@users.noreply.github.com>
2024-07-12 09:30:23 +0200

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiAppIcon202407;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiAppIcon202407;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
